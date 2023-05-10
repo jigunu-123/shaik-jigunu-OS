0307c7704184ffede6e0f14eeb11ecb15d40db7a
shaik jigunu(192111295) <113428415+jigunu-123@users.noreply.github.com>
2023-05-10 21:49:04 +0530

    greatest of 3 numbers.sh

---
+++ greatest of 3 numbers.sh
@@ -0,0 +1,20 @@
+echo enter num1
+
+read num1
+
+echo " enter num2 "
+
+read num2 echo enter num3 "
+
+read num3
+
+if [ $numi -gt $num2 ] $$ [Snum1 -gt $num3] then
+
+echo" the greatestnumber: $num1 "
+
+elif [ $num2 -gt $num3 ] $$ [ $num2 -gt Snum1]
+
+then
+
+echo " the greastest number: $num2 " else echo " the greatest number: $num3 "
+fi
